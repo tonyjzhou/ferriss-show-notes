2dd61d4c56a869364410aad82daec17747544142
Tony ZHOU <tony.zhoujingtao@gmail.com>
2020-12-22 11:52:29 -0800

    Making things more generic

--- main.py
+++ main.py
@@ -5,17 +5,30 @@ import webbrowser
 
 def main():
     if len(sys.argv) > 0:
-        base_url = sys.argv[1]
-        total_seconds = convert_time_to_seconds(sys.argv[2])
-        full_url = f"{base_url}&t={total_seconds}s"
+        full_url = convert_to_full_url(base_url=sys.argv[1], time=sys.argv[2])
 
         print(full_url)
         webbrowser.open(full_url, new=2)
 
 
+def convert_to_full_url(base_url: str, time: str):
+    total_seconds = convert_time_to_seconds(time=time)
+    full_url = f"{base_url}&t={total_seconds}s"
+    return full_url
+
+
 def convert_time_to_seconds(time: str):
-    hours, minutes, seconds = time.split(':')
-    total_seconds = int(seconds) + int(minutes) * 60 + int(hours) * 60 * 60
+    time_slice = [int(t) for t in time.split(':')]
+    if len(time_slice) <= 0:
+        hours, minutes, seconds = 0, 0, 0
+    elif len(time_slice) == 1:
+        hours, minutes, seconds = [0, 0] + time_slice
+    elif len(time_slice) == 2:
+        hours, minutes, seconds = [0] + time_slice
+    elif len(time_slice) >= 3:
+        hours, minutes, seconds = time_slice[:3]
+
+    total_seconds = seconds + minutes * 60 + hours * 60 * 60
     return total_seconds
 
 
