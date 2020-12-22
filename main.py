5b309f82d016d7fe976fb3ca737db52bac75fa34
Tony ZHOU <tony.zhoujingtao@gmail.com>
2020-12-22 11:06:37 -0800

    Enable full automation of url construction and browser openning

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 import sys
+import webbrowser
 
 
 def main():
     if len(sys.argv) > 0:
-        hours, minutes, seconds = sys.argv[1].split(':')
+        total_seconds = convert_time_to_seconds(sys.argv[1])
+        base_url = sys.argv[2]
+        full_url = f"{base_url}&t={total_seconds}s"
 
-        total_seconds = int(seconds) + int(minutes) * 60 + int(hours) * 60 * 60
+        print(full_url)
+        webbrowser.open(full_url, new=2)
 
-        print(total_seconds)
+
+def convert_time_to_seconds(time: str):
+    hours, minutes, seconds = time.split(':')
+    total_seconds = int(seconds) + int(minutes) * 60 + int(hours) * 60 * 60
+    return total_seconds
 
 
 if __name__ == '__main__':
