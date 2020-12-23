997bf6a7d7f6ee686148f5dc6b7ce16489b8d2df
Tony ZHOU <tony.zhoujingtao@gmail.com>
2020-12-22 20:55:48 -0800

    Fix title

--- make_notes.py
+++ make_notes.py
@@ -9,45 +9,53 @@ from converter import convert_to_full_url
 from model.note import Note, Notes
 
 
-def url_to_notes(blog_url: str, youtube_url: str):
+def _extract_title(soup):
+    title_tag = soup.find('h1', {"class": "entry-title"})
+    return title_tag.text
+
+
+def _make_notes(blog_url: str, youtube_url: str):
     resp = requests.get(blog_url)
 
     if resp.status_code == 200:
         soup = BeautifulSoup(resp.text, 'html.parser')
 
-        show_notes_tag = soup.find('h3', text=re.compile('SHOW NOTES'))
+        content = _make_content(soup, youtube_url)
+        title = _extract_title(soup)
 
-        notes_tag = show_notes_tag.fetchNextSiblings()[0]
-
-        note_regexp = re.compile(r'(.+) \[(.+)\]')
-
-        content = []
-        for note_tag in notes_tag.findAll('li'):
-            note_match = note_regexp.match(note_tag.text)
-
-            description, time = note_match.group(1), note_match.group(2)
-            full_url = convert_to_full_url(base_url=youtube_url, time=time)
-
-            content.append(Note(description=description, time=time, url=full_url))
-
-        return Notes(title="blah", content=content)
+        return Notes(title=title, content=content)
     else:
         return None
 
 
+def _make_content(soup, youtube_url):
+    show_notes_tag = soup.find('h3', text=re.compile('SHOW NOTES'))
+    notes_tag = show_notes_tag.fetchNextSiblings()[0]
+    note_regexp = re.compile(r'(.+) \[(.+)\]')
+    content = []
+    for note_tag in notes_tag.findAll('li'):
+        note_match = note_regexp.match(note_tag.text)
+
+        description, time = note_match.group(1), note_match.group(2)
+        full_url = convert_to_full_url(base_url=youtube_url, time=time)
+
+        content.append(Note(description=description, time=time, url=full_url))
+    return content
+
+
 def main():
-    notes = url_to_notes(blog_url='https://tim.blog/2020/12/09/harley-finkelstein/',
-                         youtube_url='https://www.youtube.com/watch?v=tcvzxXhSQ8o')
-    make_notes(notes)
+    notes = _make_notes(blog_url='https://tim.blog/2020/12/09/harley-finkelstein/',
+                        youtube_url='https://www.youtube.com/watch?v=tcvzxXhSQ8o')
+    _save_notes(notes)
 
 
-def make_notes(notes: list):
-    html = render_notes(notes)
-    with open(f"public/{notes.title}", "w") as writer:
+def _save_notes(notes: list):
+    html = _render_notes(notes)
+    with open(f"public/{notes.title}.html", "w") as writer:
         writer.writelines(html)
 
 
-def render_notes(notes: list) -> str:
+def _render_notes(notes: list) -> str:
     env = Environment(
         loader=PackageLoader(package_name="jinja"),
         autoescape=select_autoescape(["html", "xml"]),
